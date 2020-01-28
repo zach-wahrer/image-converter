c83d7490d86fb0c15ec39498f3e257d61c691a05
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-28 10:46:49 -0700

    Refactor testing structure

--- image-converter/tests/test_jpg_to_png_converter.py
+++ image-converter/tests/test_jpg_to_png_converter.py
@@ -1,16 +1,37 @@
+"""Testing for jpg_to_png_converter script."""
+
 import unittest
-from jpg_to_png_converter import input_folder_exists, verify_command_args
+from jpg_to_png_converter import (jpgs_to_convert, input_folder_exists,
+                                  verify_command_args, pathlib)
 
 
 class TestJPGToPNG(unittest.TestCase):
 
+    # Input files validation
+    def test_input_files_valid(self):
+        directory = pathlib.Path('tests/images/')
+        input = jpgs_to_convert(directory)
+        output = ['tests/images/pikachu.jpg', 'tests/images/charmander.jpg',
+                  'tests/images/astro.jpeg', 'tests/images/bulbasaur.JPG',
+                  'tests/images/squirtle.jpg']
+        self.assertEqual(input, output)
+
+    def test_input_files_empty_folder(self):
+        directory = pathlib.Path('tests/empty/')
+        input = jpgs_to_convert(directory)
+        self.assertFalse(input)
+
     # Input folder validation
     def test_input_folder_valid(self):
-        input = input_folder_exists('images/')
+        input = input_folder_exists('tests/images/')
         self.assertTrue(input)
 
     def test_input_folder_invalid_01(self):
         input = input_folder_exists('01')
+        self.assertFalse(input)
+
+    def test_input_folder_empty(self):
+        input = input_folder_exists('tests/empty')
         self.assertFalse(input)
 
     def test_input_folder_invalid_blank(self):
